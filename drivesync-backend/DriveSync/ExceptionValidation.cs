8f42a2d668448815892c488267f1608537f52844
Daniel Peralba <81268953+danielcperalba@users.noreply.github.com>
2024-08-30 14:57:36 -0300

    Implementando validação para os testes unitários

---
+++ drivesync-backend/DriveSync/ExceptionValidation.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DriveSync.Validation
+{
+    public class ExceptionValidation : Exception
+    {
+        public ExceptionValidation(string error) : base(error)
+        {
+
+        }
+
+        public static void When(bool hasError, string error)
+        {
+            if(hasError)
+            {
+                throw new ExceptionValidation(error);
+            }
+        }
+    }
+}
